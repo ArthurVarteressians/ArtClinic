54e5c9eef799f7179543885be547ffe55cf8f72e
Arthur Va <arthurvarteressian@gmail.com>
2023-06-07 14:58:47 +0400

    Yeah

--- client/src/Components/FAQS/Accordion.js
+++ client/src/Components/FAQS/Accordion.js
@@ -6,17 +6,19 @@ function Accordion({ title, content }) {
     setIsExpanded(!isExpanded);
   }
   return (
-    <div className="accordion">
-      <button className="accordion__button" onClick={handleToggle}>
-        <p className="accordion__title">{title}</p>
-        <span
-          className={`accordion__icon ${
-            isExpanded ? "accordion__icon--expanded" : ""
-          }`}
-        >
-          <i className="fas fa-plus"></i> <i className="fas fa-minus"></i>
-        </span>
-      </button>
+    <>
+      <div className="accordion">
+        <button className="accordion__button" onClick={handleToggle}>
+          <p className="accordion__title">{title}</p>
+          <span
+            className={`accordion__icon ${
+              isExpanded ? "accordion__icon--expanded" : ""
+            }`}
+          >
+            <i className="fas fa-plus"></i> <i className="fas fa-minus"></i>
+          </span>
+        </button>
+      </div>
       <div
         className={`accordion__content ${
           isExpanded ? "accordion__content--expanded" : ""
@@ -24,7 +26,7 @@ function Accordion({ title, content }) {
       >
         {content}
       </div>
-    </div>
+    </>
   );
 }
 export default Accordion;
